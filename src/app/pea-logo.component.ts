4ddb02896b735807e2c0f047b831dd21088b1c72
pittaya chonkeeree <pittaya.cho@pea.co.th>
2022-05-27 19:07:55 +0700

    commite

---
+++ src/app/pea-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'pea-logo' ,
+template : '<img id="dnn_LOGO3_imgLogo" src="/assets/pea.png" alt="การไฟฟ้าส่วนภูมิภาค - กฟภ - PEA"> <br>'
+}
+)
+
+export class PeaLogoComponent
+{
+
+}
